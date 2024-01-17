275b395b69967cae37dbe834be0bed53be53baef
CodeSandbox <support@codesandbox.io>
2024-01-17 04:51:19 +0000

    Initial commit

--- main.go
+++ main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	s "strings"
+)
+
+var printu = fmt.Println
+func ternaryu[T any](cond bool, vtrue, vfalse T) T {
+    if cond { 
+		return vtrue 
+	}
+    return vfalse
+}
 
 func main() {
-	fmt.Println("Hello, Go in CodeSandbox!")
+	fs := http.FileServer(http.Dir("./ware"))
+	http.Handle("/ware/", http.StripPrefix("/ware/", fs))
+	http.HandleFunc("/", serveTemplateu)
+
+	printu("Listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
+
+func serveTemplateu(w http.ResponseWriter, r *http.Request) {
+	lp := filepath.Join("work", "layout.html")
+
+	u := ternaryu(s.Contains(r.URL.Path, ".html"), r.URL.Path, "index.html")
+	fp := filepath.Join("work", filepath.Clean(u))
+
+	/*/ Return a 404 if the template doesn't exist /*/
+	info, err := os.Stat(fp)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.NotFound(w, r)
+			return
+		}
+	}
+
+	/*/ Return a 404 if the request is for a directory /*/
+	if info.IsDir() {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles(lp, fp)
+	if err != nil {
+		/*/ Log the detailed error /*/
+		log.Print(err.Error())
+		/*/ Return a generic "Internal Server Error" message /*/
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	err = tmpl.ExecuteTemplate(w, "layout", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, http.StatusText(500), 500)
+	}
+}
+
+
+/*/ https://www.alexedwards.net/blog/serving-static-sites-with-go /*/
---
+++ ware/hexjs.js
@@ -0,0 +1,207 @@
+/*/ HEXJS (x6)/*/
+/*/ 0.5.0 /*/
+
+(function webpackUniversalModuleDefinition(root, factory) {
+  if (typeof exports === "object" && typeof module === "object")  module.exports = factory();
+  else if (typeof define === "function" && define.amd) define("x6", [], factory);
+  else if (typeof exports === "object") exports["x6"] = factory();
+  else root["x6"] = factory();
+})( typeof self !== "undefined" ? self : typeof global !== "undefined" ? global : this, () => {
+  return (() => {
+    "use strict";
+
+    /*/ <-- Modules Function Registry /*/
+    const ms = {}; /*/ ModuleS /*/
+    ms.legacy = (v) => {
+      const { m, es, req } = v; /*/ Module, ExpotrS, REQuire /*/
+
+      req.d(es, {
+        cxyo: /*/ Mouse & Touch client x, y /*/ () => im.cxyo, 
+        geto: /*/ Get server file xml, json, svg /*/ () => im.geto,
+        // Play: (/*/ Imported Module /*/) => im.Play,
+        // Page: (/*/ Imported Module /*/) => im.Page,
+        // Part: (/*/ Imported Module /*/) => im.Part,
+        // Pack: (/*/ Imported Module /*/) => im.Pack,
+        // Plot: (/*/ Imported Module /*/) => im.Plot,
+        // Plan: (/*/ Imported Module /*/) => im.Plan,
+      });
+      const im /*/ Imported Module /*/ = req("commom"); 
+
+      const globalObject = typeof req.g !== "undefined" ? req.g : typeof window !== "undefined" ? window : undefined;
+      if (typeof globalObject !== "undefined") {
+        globalObject.x6 = globalObject.x6 || {};
+        const HEXGLOBAL = globalObject.x6;
+        for (let key in im) { if (!HEXGLOBAL[key]) HEXGLOBAL[key] = im[key]; }
+      }
+    };
+  
+    /*/ <-- Modules Function Structure /*/
+    ms.commom = (v) => {
+      const { m, es, req } = v; /*/ Module, ExpotrS, REQuire /*/
+
+      req.r(es);
+      req.d(es, {
+        cxyo: () => cxyo,
+        geto: () => geto,
+        // Page: () => Page,
+        // Part: () => Part,
+        // Pack: () => Pack,
+        // Plot: () => Plot,
+        // Plan: () => Plan,
+      });
+
+      /*/ <-- Mouse & Touch client x, y /*/
+      const cxyo = function () {
+        this.e = { start: '', move: '', end: '' };
+        this.xya = [ /*/{ x: 0, y: 0 } /*/ ];
+        this.popu = () => this.xya.length > 0 ? this.xya.pop() : null;
+        
+        this.eventu = v => {
+          const { b, n } = v; /*/ b: boolean, n: number /*/
+
+          if(b){
+            v.m = navigator.userAgentData.mobile;
+            if(v.m) {
+              this.e.start = 'touchstart';
+              this.e.move = 'touchmove';
+              this.e.end = 'touchend';
+
+            } else {
+              this.e.start = 'mousedown';
+              this.e.move = 'mousemove';
+              this.e.end = 'mouseup';
+            }
+    
+            const addue = e => addu({ e: e });
+            const addu = v => {
+              const { e } = v;
+
+              if(this.xya.length > n && n) this.popu();
+              this.xya.unshift({ x: e.clientX || e.touches[0].clientX, y: e.clientY || e.touches[0].clientY });
+            }          
+            
+            document.addEventListener(this.e.start, addue);
+            document.addEventListener(this.e.move, addue);
+            document.addEventListener(this.e.end, addue);
+
+          } else {
+            document.removeEventListener(this.e.start, addue);
+            document.removeEventListener(this.e.move, addue);
+            document.removeEventListener(this.e.end, addue);
+          }
+        }
+      };
+      /*/ --> Mouse & Touch client x, y /*/
+
+      /*/ <-- Get server file /*/
+      const geto = function () {
+        const mineu = {
+          'xml': v => {
+            const { d } = v; /*/ s: XMLDocument /*/
+
+            [].forEach.call(d.querySelectorAll('svg'), e => {
+              console.log(e);
+            });
+          },
+          'json': v => {},
+          'svg+xml': v => {},
+          'html': v => {},
+          'plain': v => {}
+        }
+
+        const mimeu = {
+          'xml': 'application/xml',
+          'json': 'application/json',
+          'svg+xml': 'image/svg+xml',
+          'html': 'text/html',
+          'plain': 'text/plain' 
+        }
+
+        this.file = async(v) => {
+          const { u, n, m } = v; /*/ u: string, n: string, m: string /*/
+
+          v.m = '';
+          v.r = await fetch ([u, n].join('/') /*/ url /*/);
+          v.d = await v.r.text().then(e => {
+            return new DOMParser().parseFromString(e, mimeu[m]);
+          });
+         
+          mineu['svg+xml']({ d: v.d });
+        }
+      };
+      /*/ --> get server file /*/
+      
+
+    };
+    /*/ --> Modules Function Structure /*/
+
+    /*/ <-- Modules Function Define /*/
+    let mc = {}; /*/ Modules Cache /*/
+    const req = (id) => {
+      /*/ REQuire /*/
+      // let cachedModule = mc[id];
+      // if (cachedModule !== undefined) return cachedModule.exports;
+
+      let m = (mc[id] = { es: {} }); /*/ Module, ExpotrS /*/
+      ms[id]({ m: m, es: m.es, req: req });
+
+      return m.es;
+    };
+
+    (() => {
+      req.d = (es, def) => {
+        for (var key in def) {
+          if (req.o(def, key) && !req.o(es, key)) {
+            Object.defineProperty(es, key, {
+              enumerable: true,
+              get: def[key],
+            });
+          }
+        }
+      };
+    })();
+
+    (() => {
+      req.g = (function () {
+        if (typeof globalThis === "object") return globalThis;
+        try {
+          return this || new Function("return this")();
+        } catch (e) {
+          if (typeof window === "object") return window;
+        }
+      })();
+    })();
+
+    (() => {
+      req.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
+    })();
+
+    (() => {
+      req.r = (es) => {
+        if (typeof Symbol !== "undefined" && Symbol.toStringTag) {
+          Object.defineProperty(es, Symbol.toStringTag, {
+            value: "Module",
+          });
+        }
+        // Object.defineProperty(es, '_esModule', { value: true });
+      };
+    })();
+
+    let es = {}; /*/ ExportS /*/
+    (() => {
+      req.r(es);
+      req.d(es, {
+        core: (/*/ Imported Module Legacy /*/) => iml,
+        default: (/*/ Default Export /*/) => de,
+      });
+
+      const iml = req("legacy");
+      const de = iml;
+    })();
+   
+    es = es["default"];
+    /*/ --> Modules Function Define /*/
+
+    return es;
+    })();}
+);
